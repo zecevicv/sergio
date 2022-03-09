d3f95fd8cbd92d397b342098e35d2e9c40223a0f
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2022-03-09 13:07:07 +0100

    [ADD] - Course Page

--- js/app.js
+++ js/app.js
@@ -34,29 +34,95 @@ AOS.init({
 
 /* #Blog Slider
   ======================================================= */
-  const blogSliders = document.querySelectorAll('.blog-slider');
+const blogSliders = document.querySelectorAll('.blog-slider');
 
-  if (blogSliders) {
-    blogSliders.forEach((blogSlider) => {
-      const sliderEl = blogSlider.querySelector('.swiper');
-      const sliderPagination = blogSlider.querySelector('.swiper-pagination');
-  
-      new Swiper(sliderEl, {
-        breakpoints: {
-          0: {
-            slidesPerView: 1.128
-          },
-          1024: {
-            slidesPerView: 3.25
-          },
+if (blogSliders) {
+  blogSliders.forEach((blogSlider) => {
+    const sliderEl = blogSlider.querySelector('.swiper');
+    const sliderPagination = blogSlider.querySelector('.swiper-pagination');
+
+    new Swiper(sliderEl, {
+      breakpoints: {
+        0: {
+          slidesPerView: 1.128
         },
-        pagination: {
-          el: sliderPagination,
-          type: "progressbar",
+        1024: {
+          slidesPerView: 3.25
         },
-        
-      });
-  
-  
+      },
+      pagination: {
+        el: sliderPagination,
+        type: "progressbar",
+      },
+
     });
-  }
+
+
+  });
+}
+
+/* #Counter
+  ======================================================= */
+if (document.querySelector('.counter')) {
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach((counter) => {
+    const hours = counter.querySelector("#hours");
+    const minutes = counter.querySelector("#minutes");
+    const seconds = counter.querySelector("#seconds");
+    const startBtn = counter.querySelector("#startBtn");
+    let mySecond = 45;
+    let myMinute = 30;
+    let myHour = 15;
+    var timer = null;
+
+    function startTimer() {
+      mySecond <= 10 ?
+        (seconds.innerHTML = "0" + mySecond) :
+        (seconds.innerHTML = mySecond);
+      myMinute <= 10 ?
+        (minutes.innerHTML = "0" + myMinute) :
+        (minutes.innerHTML = myMinute);
+      myHour <= 10 ? (hours.innerHTML = "0" + myHour) : (hours.innerHTML = myHour);
+      timer = setInterval(() => {
+        if (mySecond == 0) {
+          mySecond = 60;
+          if (myMinute != 0) {
+            myMinute <= 10 ?
+              (minutes.innerHTML = "0" + --myMinute) :
+              (minutes.innerHTML = --myMinute);
+          } else if (myHour != 0) {
+            myHour <= 10 ?
+              (hours.innerHTML = "0" + --myHour) :
+              (hours.innerHTML = --myHour);
+            myMinute = 60;
+            minutes.innerHTML = --myMinute;
+          } else {
+            alert("Time's up!");
+            clearInterval(timer);
+            startBtn.removeAttribute("disabled");
+          }
+        }
+        mySecond <= 10 ?
+          (seconds.innerHTML = "0" + --mySecond) :
+          (seconds.innerHTML = --mySecond);
+      }, 1000);
+    }
+
+    startTimer();
+  });
+}
+
+/* #Sticky Desktop Button Course Page
+    ======================================================= */
+if (document.querySelector('.course-page .sticky-button-desktop')) {
+  const courseBanner = document.querySelector('.course-banner');
+  const stickyButtonDesktop = document.querySelector('.sticky-button-desktop');
+  document.addEventListener('scroll', (e) => {
+    if (!isInViewport(courseBanner)) {
+      stickyButtonDesktop.classList.add('show');
+    } else {
+      stickyButtonDesktop.classList.remove('show');
+    }
+  })
+}
